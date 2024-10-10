324ed30e6423b9551bbf7f5724afb28b00ae2757
sergiofr13 <sergio.ferreiro.ramos@gmail.com>
2024-10-10 12:52:02 +0200

    Versión 1.0 Suma.java

--- ud01/ciclovida/Suma.java
+++ ud01/ciclovida/Suma.java
@@ -7,18 +7,18 @@ public class Suma {
         Scanner scanner = new Scanner(System.in);
 
         // Solicitar al usuario el primer número
-        System.out.print("Ingrese el primer número: ");
+       
         double num1 = scanner.nextDouble();
 
         // Solicitar al usuario el segundo número
-        System.out.print("Ingrese el segundo número: ");
+        
         double num2 = scanner.nextDouble();
 
         // Calcular la suma
         double suma = num1 + num2;
 
         // Imprimir el resultado
-        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+        System.out.println(suma);
 
         // Cerrar el escáner
         scanner.close();
@@ -26,17 +26,5 @@ public class Suma {
 }
     
 
-public class Suma {
-    public static void main (String[] args) {
-        double numero1 = Double.parseDouble(args[0]);
-        double numero2 = Double.parseDouble(args[1]);
-
-        double suma = numero1 + numero2;
-
-        System.out.println(suma);
-    }
-
-    
-}
 
 
---
+++ ud01/ciclovida/probas.md
@@ -0,0 +1,35 @@
+### Proba 1
+
+
+Entrada
+
+    java Suma 3.4 7.6
+
+Output
+
+    11.0
+
+### Proba 2
+
+java Suma
+-5
+-8
+-13.0
+
+
+### Proba 3
+
+java Suma 
+65263432543623657
+254234648356842623525235
+2.5423471362027517E23
+
+### Proba 4
+
+java Suma 
+dasda
+Exception in thread "main" java.util.InputMismatchException
+        at java.base/java.util.Scanner.throwFor(Scanner.java:947)
+        at java.base/java.util.Scanner.next(Scanner.java:1602)
+        at java.base/java.util.Scanner.nextDouble(Scanner.java:2573)
+        at Suma.main(Suma.java:11)
